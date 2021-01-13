e0f2755614e1ecbd3bf02475d7d0a1b4f2d97838
devSeungGwan <yanhis97531@gmail.com>
2021-01-13 15:45:02 +0900

    Update: 개발토끼 10장 강의
    - 로그인 기능 구현
    - 로그인 시 필요한 Validation Check를 위해 ModelView에 Model 생성
    - Validation Check 시 Equals 사용

--- ASPnetNote.MVC6/Controllers/AccountController.cs
+++ ASPnetNote.MVC6/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using ASPnetNote.MVC6.DataContext;
 using ASPnetNote.MVC6.Models;
+using ASPnetNote.MVC6.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -20,8 +21,12 @@ namespace ASPnetNote.MVC6.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
-        public IActionResult Login(User model)
+        public IActionResult Login(LoginViewModel model)
         {
             // ID, Password: 필수
             if (ModelState.IsValid)
@@ -30,15 +35,27 @@ namespace ASPnetNote.MVC6.Controllers
                 {
                     // Linq - 메서드 체어닝(Method Chaining)
                     // => : A go to B
+
+                    // 해당 방식으로 작성 시 메모리 누수가 발생하여 성능 상 이득을 가져가기 힘듬.
                     //var user = db.Users
                     //    .FirstOrDefault(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
                     
-                    var user = db.Users
-                        .FirstOrDefault(
-                            u => u.UserId.Equals(model.UserId) &&
-                            u.UserPassword.Equals(model.UserPassword)
+                    // Equals 메소드를 사용하여 내용 비교 시 메모리 누수가 발생하지 않도록 한다.
+                    var user = db.Users.FirstOrDefault(
+                        u => u.UserId.Equals(model.UserId) &&
+                        u.UserPassword.Equals(model.UserPassword)
                         );
+
+                    if (user.Equals(null))
+                    {
+                        // 로그인에 성공했을 때
+                        return RedirectToAction("LoginSuccess", "Home");
+                    }
                 }
+
+                // 로그인에 실패했을 때
+                ModelState.AddModelError(string.Empty, "사용자 ID 혹은 비밀번호가 올바르지 않습니다.");
+
             }
 
 
