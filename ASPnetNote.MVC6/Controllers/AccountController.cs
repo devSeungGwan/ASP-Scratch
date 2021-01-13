0bdef52363147206941b63ecc7abb3f6b4adb119
devSeungGwan <yanhis97531@gmail.com>
2021-01-13 14:56:11 +0900

    Update: 개발토끼 9장 강의
    - 회원가입 페이지를 구축함으로 MSSQL과 CodeFirst 방식으로 연결
    - ORM 및 페이지 구축 관련 학습

---
+++ ASPnetNote.MVC6/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using ASPnetNote.MVC6.DataContext;
+using ASPnetNote.MVC6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPnetNote.MVC6.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+            // ID, Password: 필수
+            if (ModelState.IsValid)
+            {
+                using (var db = new ASPnetNoteDbContext())
+                {
+                    // Linq - 메서드 체어닝(Method Chaining)
+                    // => : A go to B
+                    //var user = db.Users
+                    //    .FirstOrDefault(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
+                    
+                    var user = db.Users
+                        .FirstOrDefault(
+                            u => u.UserId.Equals(model.UserId) &&
+                            u.UserPassword.Equals(model.UserPassword)
+                        );
+                }
+            }
+
+
+            return View(model);
+        }
+
+
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Java try(sqlSession){} catch(){}
+                // C#
+                using (var db = new ASPnetNoteDbContext())
+                {
+                    db.Users.Add(model);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
--- ASPnetNote.MVC6/Controllers/HomeController.cs
+++ ASPnetNote.MVC6/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ASPnetNote.MVC6.Controllers
             return View();
         }
 
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
